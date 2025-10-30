168bd18456c33116dc06ea77f3a66622e65ccfcb
Brian Pham <brianlpham21@gmail.com>
2025-10-30 09:37:52 -0700

    testing with no config for i18n

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  i18n: {
-    locales: ['en', 'ja', 'vi'], // English, Japanese, Vietnamese
-    defaultLocale: 'en',
-  },
   // Other Next.js config options can go here
 };
 
