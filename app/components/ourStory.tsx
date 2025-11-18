c4134aa32609ca8ed049444e9ec4f026da9862ad
Brian Pham <brianlpham21@gmail.com>
2025-11-18 14:18:03 -0800

    our story photo and styling

--- app/components/ourStory.tsx
+++ app/components/ourStory.tsx
@@ -43,14 +43,13 @@ export default function OurStory({ selectedLang }: { selectedLang: { code: strin
       </motion.div>
 
       {/* Circular Cutout Image with Shadow */}
-      <motion.div
+      {/* <motion.div
         initial={{ opacity: 0, x: 50 }}
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true, amount: 0.2 }}
         transition={{ duration: 1.2, ease: 'easeOut' }}
         className="flex-1 max-w-xs relative z-10"
       >
-        {/* Circle container with hover animation */}
         <motion.div
           whileHover={{ scale: 1.01, boxShadow: '0 10px 15px rgba(0,0,0,0.3)' }}
           transition={{ duration: 1, ease: 'easeOut' }}
@@ -62,6 +61,50 @@ export default function OurStory({ selectedLang }: { selectedLang: { code: strin
             fill
             className="object-cover"
             sizes="(max-width: 768px) 16rem, 18rem"
+            priority
+          />
+        </motion.div>
+      </motion.div> */}
+      {/* Image Column */}
+      <motion.div
+        initial={{ opacity: 0, x: 50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: true, amount: 0.2 }}
+        transition={{ duration: 1.2, ease: 'easeOut' }}
+        className="flex-1 max-w-xs relative z-10 flex flex-col items-center"
+      >
+        {/* Circle Image */}
+        <motion.div
+          whileHover={{ scale: 1.02, boxShadow: '10px 20px 15px rgba(0,0,0,.7)' }}
+          transition={{ duration: 1, ease: 'easeOut' }}
+          style={{ boxShadow: '10px 20px 15px rgba(0,0,0,0.5)' }}
+          className="w-64 h-64 md:w-72 md:h-72 rounded-full overflow-hidden relative shadow-md transition-shadow duration-300 ease-out mr-0 md:mr-30 z-1"
+        >
+          <Image
+            src="/photoshoot-ring.png"
+            alt="Mio & Brian"
+            fill
+            className="object-cover scale-125"
+            sizes="(max-width: 768px) 16rem, 18rem"
+            priority
+          />
+        </motion.div>
+
+        {/* Rectangular Image Below, Slightly Staggered */}
+        <motion.div
+          initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true, amount: 0.2 }}
+          whileHover={{ scale: 1.02 }} // zoom on hover
+          transition={{ duration: 1.2, ease: 'easeOut', delay: 0.3 }}
+          className="w-80 md:w-90 h-90 md:h-110 rounded-xl overflow-hidden shadow-md -mt-40 relative"
+        >
+          <Image
+            src="/photoshoot-window.jpg"
+            alt="Wedding detail"
+            fill
+            className="object-cover scale-120"
+            style={{ objectPosition: '50% 10%' }} // controls visible area
             priority
           />
         </motion.div>
